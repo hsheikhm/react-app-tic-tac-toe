d36f6567d3c606fff69687390d24ea5712a3bc4d
Hamza Sheikh <hsheikhm@gmail.com>
2016-11-28 18:36:41 +0000

    created Square component

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+"use strict";
+
+import React from 'react';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
